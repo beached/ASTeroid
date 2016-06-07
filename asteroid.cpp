920d3179123b4590a6e95db518964ad7f0640262
Darrell Wright <dwright-git@dawdevel.ca>
2016-06-07 17:57:10 -0400

    .

--- asteroid.cpp
+++ asteroid.cpp
@@ -24,61 +24,79 @@
 
 namespace daw {
 	namespace asteroid {
-		ast_node::ast_node( ast_node_type node_type ): 
-			m_node_type{ node_type } { }
+		ast_node::ast_node( ast_node_type node_type ):
+			m_node_type { node_type } { }
 
-		ast_node::ast_node_type const& ast_node::node_type() const {
+		ast_node::ast_node_type const& ast_node::node_type( ) const {
 			return m_node_type;
 		}
 
 		ast_node::~ast_node( ) { }
 
-		ast_node_scope::ast_node_scope( ): 
-			ast_node { ast_node::ast_node_type::scope }, 
+		ast_node_scope::ast_node_scope( ):
+			ast_node { ast_node::ast_node_type::scope },
 			children { } { }
 
-		ast_node_type_identifier::ast_node_type_identifier( std::string type_name ): 
-			ast_node { ast_node::ast_node_type::type_identifier }, 
-			name { std::move( type_name ) }, 
-			width { 0 }, 
+		ast_node_scope::~ast_node_scope( ) { }
+
+		ast_node_type_identifier::ast_node_type_identifier( std::string type_name ):
+			ast_node { ast_node::ast_node_type::type_identifier },
+			name { std::move( type_name ) },
+			width { 0 },
 			components { } { }
+
+		ast_node_type_identifier::~ast_node_type_identifier( ) { }
 
 		addressable::~addressable( ) { }
 
 		has_value::~has_value( ) { }
 
 		ast_node_cpu_register::ast_node_cpu_register( std::string register_name ):
-			ast_node { ast_node::ast_node_type::cpu_register }, 
+			ast_node { ast_node::ast_node_type::cpu_register },
 			name { std::move( register_name ) } { }
 
-		ast_node_memory_address::ast_node_memory_address(uintmax_t memory_location):
+		ast_node_cpu_register::~ast_node_cpu_register( ) { }
+
+		ast_node_memory_address::ast_node_memory_address( uintmax_t memory_location ):
 			ast_node { ast_node::ast_node_type::memory_address },
 			location { memory_location } { }
+
+		ast_node_memory_address::~ast_node_memory_address( ) { }
 
 		assignable::~assignable( ) { }
 
 		ast_node_variable_declaration::ast_node_variable_declaration( std::shared_ptr<ast_node_type_identifier> variable_type_id, std::string variable_name ):
 			ast_node { ast_node::ast_node_type::variable_declaration },
-			location{ },
-			name{ std::move( variable_name ) },
-			variable_type{ variable_type_id }  { }
+			location { },
+			name { std::move( variable_name ) },
+			variable_type { variable_type_id } { }
+
+		ast_node_variable_declaration::~ast_node_variable_declaration( ) { }
 
 		ast_node_assignement::ast_node_assignement( std::shared_ptr<assignable> left, std::shared_ptr<has_value> right ):
 			ast_node { ast_node::ast_node_type::assignment },
-			lhs{ std::move( left ) },
-			rhs{ std::move( right ) } { }
+			lhs { std::move( left ) },
+			rhs { std::move( right ) } { }
+
+		ast_node_assignement::~ast_node_assignement( ) { }
 
 		ast_node_loop::ast_node_loop( std::shared_ptr<ast_node_scope> loop_body ):
 			ast_node { ast_node::ast_node_type::loop },
-			scope{ std::move( loop_body ) } { }
+			scope { std::move( loop_body ) } { }
 
-		ast_node_label::ast_node_label(std::string label_name):
+		ast_node_loop::~ast_node_loop( ) { }
+
+		ast_node_label::ast_node_label( std::string label_name ):
 			ast_node { ast_node::ast_node_type::label },
-			identifier{ std::move( label_name ) } { }
+			identifier { std::move( label_name ) } { }
+
+		ast_node_label::~ast_node_label( ) { }
 
 		ast_node_jump::ast_node_jump( std::shared_ptr<ast_node_label> destination ):
 			ast_node { ast_node::ast_node_type::jump },
-			label{ std::move( destination ) } { }
+			label { std::move( destination ) } { }
+
+		ast_node_jump::~ast_node_jump( ) { }
 
 		ast_node_operator_add::ast_node_operator_add( std::shared_ptr<assignable> Result, std::shared_ptr<has_value> Left, std::shared_ptr<has_value> Right ):
 			ast_node { ast_node::ast_node_type::operator_add },
@@ -86,11 +104,15 @@ namespace daw {
 			lhs { std::move( Left ) },
 			rhs { std::move( Right ) } { }
 
+		ast_node_operator_add::~ast_node_operator_add( ) { }
+
 		ast_node_operator_sub::ast_node_operator_sub( std::shared_ptr<assignable> Result, std::shared_ptr<has_value> Left, std::shared_ptr<has_value> Right ):
 			ast_node { ast_node::ast_node_type::operator_sub },
 			result { std::move( Result ) },
 			lhs { std::move( Left ) },
 			rhs { std::move( Right ) } { }
+
+		ast_node_operator_sub::~ast_node_operator_sub( ) { }
 
 		ast_node_operator_mul::ast_node_operator_mul( std::shared_ptr<assignable> Result, std::shared_ptr<has_value> Left, std::shared_ptr<has_value> Right ):
 			ast_node { ast_node::ast_node_type::operator_mul },
@@ -98,13 +120,14 @@ namespace daw {
 			lhs { std::move( Left ) },
 			rhs { std::move( Right ) } { }
 
+		ast_node_operator_mul::~ast_node_operator_mul( ) { }
+
 		ast_node_operator_div::ast_node_operator_div( std::shared_ptr<assignable> Result, std::shared_ptr<has_value> Left, std::shared_ptr<has_value> Right ):
 			ast_node { ast_node::ast_node_type::operator_div },
 			result { std::move( Result ) },
 			lhs { std::move( Left ) },
 			rhs { std::move( Right ) } { }
 
-
+		ast_node_operator_div::~ast_node_operator_div( ) { }
 	}	// namespace asteroid
 }	// namespace daw
-
--- include/asteroid.h
+++ include/asteroid.h
@@ -29,8 +29,10 @@
 namespace daw {
 	namespace asteroid {
 		struct ast_node: public std::enable_shared_from_this<ast_node> {
-			enum class ast_node_type { scope, type_identifier, memory_address, cpu_register, variable_declaration, assignment, loop, label, jump,
-										operator_add, operator_sub, operator_mul, operator_div };
+			enum class ast_node_type {
+				scope, type_identifier, memory_address, cpu_register, variable_declaration, assignment, loop, label, jump,
+				operator_add, operator_sub, operator_mul, operator_div
+			};
 
 		protected:
 			ast_node_type m_node_type;
@@ -51,7 +53,7 @@ namespace daw {
 
 			ast_node_scope( );
 			~ast_node_scope( );
-			
+
 			ast_node_scope( ast_node_scope const & ) = default;
 			ast_node_scope( ast_node_scope && ) = default;
 			ast_node_scope & operator=( ast_node_scope const & ) = default;
@@ -71,7 +73,7 @@ namespace daw {
 			ast_node_type_identifier & operator=( ast_node_type_identifier const & ) = default;
 			ast_node_type_identifier & operator=( ast_node_type_identifier && ) = default;
 		};	// ast_node_type_identifier
-		
+
 		struct addressable {
 			virtual ~addressable( );
 		};	// struct addressable
@@ -94,13 +96,14 @@ namespace daw {
 
 		struct ast_node_memory_address: public ast_node, public addressable, public has_value {
 			uintmax_t location;
+
 			ast_node_memory_address( uintmax_t memory_location );
+			~ast_node_memory_address( );
 
 			ast_node_memory_address( ast_node_memory_address const & ) = default;
 			ast_node_memory_address( ast_node_memory_address && ) = default;
 			ast_node_memory_address & operator=( ast_node_memory_address const & ) = default;
 			ast_node_memory_address & operator=( ast_node_memory_address && ) = default;
-			~ast_node_memory_address( ) = default;
 		};	// ast_node_memory_address
 
 		struct assignable {
@@ -112,63 +115,63 @@ namespace daw {
 			std::string name;
 			std::shared_ptr<ast_node_type_identifier> variable_type;
 
-			ast_node_variable_declaration( std::shared_ptr<ast_node_type_identifier> variable_type_id, std::string variable_name );				
+			ast_node_variable_declaration( std::shared_ptr<ast_node_type_identifier> variable_type_id, std::string variable_name );
+			~ast_node_variable_declaration( );
 
 			ast_node_variable_declaration( ast_node_variable_declaration const & ) = default;
 			ast_node_variable_declaration( ast_node_variable_declaration && ) = default;
 			ast_node_variable_declaration & operator=( ast_node_variable_declaration const & ) = default;
 			ast_node_variable_declaration & operator=( ast_node_variable_declaration && ) = default;
-			~ast_node_variable_declaration( ) = default;
+
 		};	// ast_node_variable_declaration
 
 		struct ast_node_assignement: public ast_node {
 			std::shared_ptr<assignable> lhs;
 			std::shared_ptr<has_value> rhs;
+
 			ast_node_assignement( std::shared_ptr<assignable> left, std::shared_ptr<has_value> right );
+			~ast_node_assignement( );
 
 			ast_node_assignement( ast_node_assignement const & ) = default;
 			ast_node_assignement( ast_node_assignement && ) = default;
 			ast_node_assignement & operator=( ast_node_assignement const & ) = default;
 			ast_node_assignement & operator=( ast_node_assignement && ) = default;
-			~ast_node_assignement( ) = default;
 		};	// ast_node_assignement
-	
+
 		struct ast_node_loop: public ast_node {
 			std::shared_ptr<ast_node_scope> scope;
 
 			ast_node_loop( std::shared_ptr<ast_node_scope> loop_body );
-
+			~ast_node_loop( );
 
 			ast_node_loop( ast_node_loop const & ) = default;
 			ast_node_loop( ast_node_loop && ) = default;
 			ast_node_loop & operator=( ast_node_loop const & ) = default;
 			ast_node_loop & operator=( ast_node_loop && ) = default;
-			~ast_node_loop( ) = default;
 		};	// ast_node_loop
 
 		struct ast_node_label: public ast_node {
 			std::string identifier;
 
 			ast_node_label( std::string label_name );
-			
-			
+			~ast_node_label( );
+
 			ast_node_label( ast_node_label const & ) = default;
 			ast_node_label( ast_node_label && ) = default;
 			ast_node_label & operator=( ast_node_label const & ) = default;
 			ast_node_label & operator=( ast_node_label && ) = default;
-			~ast_node_label( ) = default;
 		};	// ast_node_label
 
-		struct ast_node_jump: public ast_node {			
+		struct ast_node_jump: public ast_node {
 			std::shared_ptr<ast_node_label> label;
 
 			ast_node_jump( std::shared_ptr<ast_node_label> destination );
+			~ast_node_jump( );
 
 			ast_node_jump( ast_node_jump const & ) = default;
 			ast_node_jump( ast_node_jump && ) = default;
 			ast_node_jump & operator=( ast_node_jump const & ) = default;
 			ast_node_jump & operator=( ast_node_jump && ) = default;
-			~ast_node_jump( ) = default;
 		};	// ast_node_jump
 
 		struct ast_node_operator_add: public ast_node {
@@ -177,12 +180,12 @@ namespace daw {
 			std::shared_ptr<has_value> rhs;
 
 			ast_node_operator_add( std::shared_ptr<assignable> Result, std::shared_ptr<has_value> Left, std::shared_ptr<has_value> Right );
+			~ast_node_operator_add( );
 
 			ast_node_operator_add( ast_node_operator_add const & ) = default;
 			ast_node_operator_add( ast_node_operator_add && ) = default;
 			ast_node_operator_add & operator=( ast_node_operator_add const & ) = default;
 			ast_node_operator_add & operator=( ast_node_operator_add && ) = default;
-			~ast_node_operator_add( ) = default;
 		};	// ast_node_operator_add
 
 		struct ast_node_operator_sub: public ast_node {
@@ -191,12 +194,12 @@ namespace daw {
 			std::shared_ptr<has_value> rhs;
 
 			ast_node_operator_sub( std::shared_ptr<assignable> Result, std::shared_ptr<has_value> Left, std::shared_ptr<has_value> Right );
+			~ast_node_operator_sub( );
 
 			ast_node_operator_sub( ast_node_operator_sub const & ) = default;
 			ast_node_operator_sub( ast_node_operator_sub && ) = default;
 			ast_node_operator_sub & operator=( ast_node_operator_sub const & ) = default;
 			ast_node_operator_sub & operator=( ast_node_operator_sub && ) = default;
-			~ast_node_operator_sub( ) = default;
 		};	// ast_node_operator_sub
 
 		struct ast_node_operator_mul: public ast_node {
@@ -205,12 +208,12 @@ namespace daw {
 			std::shared_ptr<has_value> rhs;
 
 			ast_node_operator_mul( std::shared_ptr<assignable> Result, std::shared_ptr<has_value> Left, std::shared_ptr<has_value> Right );
+			~ast_node_operator_mul( );
 
 			ast_node_operator_mul( ast_node_operator_mul const & ) = default;
 			ast_node_operator_mul( ast_node_operator_mul && ) = default;
 			ast_node_operator_mul & operator=( ast_node_operator_mul const & ) = default;
 			ast_node_operator_mul & operator=( ast_node_operator_mul && ) = default;
-			~ast_node_operator_mul( ) = default;
 		};	// ast_node_operator_mul
 
 		struct ast_node_operator_div: public ast_node {
@@ -219,17 +222,13 @@ namespace daw {
 			std::shared_ptr<has_value> rhs;
 
 			ast_node_operator_div( std::shared_ptr<assignable> Result, std::shared_ptr<has_value> Left, std::shared_ptr<has_value> Right );
+			~ast_node_operator_div( );
 
 			ast_node_operator_div( ast_node_operator_div const & ) = default;
 			ast_node_operator_div( ast_node_operator_div && ) = default;
 			ast_node_operator_div & operator=( ast_node_operator_div const & ) = default;
 			ast_node_operator_div & operator=( ast_node_operator_div && ) = default;
-			~ast_node_operator_div( ) = default;
 		};	// ast_node_operator_div
-
-
-
-
 	}	// namespace asteroid
 }	// namespace daw
 
